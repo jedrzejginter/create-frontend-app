f6fae1ddb12934ee97df569c0204729cc3a7d7b2
Jędrzej Ginter <jedrzej.ginter@monterail.com>
2020-10-16 21:27:41 +0200

    [feat] Add cli for creating project (#3)

---
+++ build/cli.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+const yargs = require('yargs/yargs');
+const createReactProject = require('./core');
+
+const argv = yargs(yargs.hideBin(process.argv))
+  .locale('en')
+  .option('out', {
+    alias: 'o',
+    type: "string",
+    demandOption: true,
+    description: "Output directory for project files",
+  })
+  .option("name", {
+    alias: 'n',
+    type: 'string',
+    demandOption: true,
+    description: 'Project name to set in package.json'
+  })
+  .option("force", {
+    alias: 'f',
+    type: 'boolean',
+    default: false,
+    description: 'Allow writing to non-empty directory (specified by --out)'
+  })
+  .argv;
+
+(async() => {
+  try {
+    await createReactProject(argv);
+    process.exit(0);
+  } catch (err) {
+    console.error(err)
+    process.exit(1);
+  }
+})()
--- visitor.js
+++
@@ -1,3 +0,0 @@
-// const t = require('@babel/types')
-
-module.exports = {}
