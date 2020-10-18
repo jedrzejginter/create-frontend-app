d02d2922bc33eb408362da186f3722c125eb2dec
Jedrzej Ginter <jedrzej.ginter@monterail.com>
2020-10-18 22:29:27 +0200

    Fix jest misconfiguration in template

---
+++ template/src/pages/Home/__tests__/Home.spec.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Home from '../Home'
+
+describe("Home", () => {
+  it('renders without errors', () => {
+    expect(() => render(<Home />)).not.toThrow()
+  })
+})
